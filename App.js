07c866449b29570906d8a13490717ad77dd8ab36
ines-ux <65893945+ines-ux@users.noreply.github.com>
2020-07-17 16:35:00 +0200

    vendredi

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.js/app.css';
+
+import Counter from './Counter.js';
+class App extends React.Component {
+    constructor(){
+        super();
+        this.IncrementCount= this.IncrementCount.bind(this);
+        this.decrementCount= this.decrementCount.bind(this);
+        // a partir de ce moment on explique qu'il faut ajouter et diminuer
+
+    }
+    state ={
+        count= 1
+    }
+    IncrementCount=() => {
+        this.setState({
+            count: this.state.count+1
+        })
+        // incrementation
+    }
+    decrementCount=() =>{
+        if ( this.state.count > 0){
+            this.setState({
+                count: this.count - 1
+            })
+        
+        }
+        // decrementation
+    }
+    render(){
+        return(
+            <div className='App'>
+                <h1>Counter</h1>
+                <Counter count={ this.state.count}
+                IncrementFn={this.IncrementCount}
+                decrementFn={this.decrementCount}/>
+            </div>
+        )
+    }
+    // rendu d'affichage
+}
+export default App;
---
+++ Counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class Counter extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2> {this.props.count}</h2>
+				<button onClick={this.props.incrementFn}>+</button>
+				<button onClick={this.props.decrementFn}>-</button>
+			</div>
+			// ajout de boutons + et -
+		);
+	}
+}
+export default Counter;
